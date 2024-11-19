ba737deb784fdc3b67a0b74d3a7c6b6e1b97ecb3
MuriloBarros304 <murilobarros304@gmail.com>
2024-11-19 16:36:17 -0300

    searching funcionando

--- tello_control.py
+++ tello_control.py
@@ -5,35 +5,35 @@ import time
 old_move = ''
 pace = ' 20'
 pace_moves = ['up', 'down', 'left', 'right', 'forward', 'back']
-
 start_time = time.time()  # Inicializa o tempo
-searching = False  # Indica se a rotina de busca está ativa
+searching = False
 
 def timer():
     '''
-    Retorna True se o tempo desde a última detecção for maior que 10 segundos.
+    Função que verifica se o timer de 5 segundos expirou.
+    Returns:
+        Booleano que indica se o timer expirou.
     '''
     global start_time
-    if time.time() - start_time >= 10:
+    if time.time() - start_time >= 5:
         start_time = time.time()  # Reinicia o timer
         return True
     return False
 
-def search(tello, frame, detections):
+def search(tello, frame):
     '''
     Função de busca que é chamada quando não há detecção de QR code após o timer expirar.
-    Executa uma rotação para procurar QR codes até que haja uma detecção.
+    Executa uma rotação para procurar QR codes.
+    Args:
+        tello: Objeto da classe TelloZune, que possui métodos para enviar comandos e obter estado.
+        frame: Frame de vídeo a ser processado para detecção de QR codes.
+    Returns:
+        frame: Frame processado após a execução da rotação
     '''
-    print("Buscando QR code...")
-
-    if detections > 0:
-        print(f"Detecção encontrada")
-        return frame, True  # Retorna o frame e indica que encontrou o QR code
-
-    response = tello.send_cmd_return('cw 20')  # Rotaciona 20 graus
-    print(response)
-    time.sleep(0.1)  # Pequena pausa para evitar sobrecarga
-    return frame, False  # Retorna o frame e False, indicando que ainda não encontrou nada
+    response = tello.send_cmd('ccw 20')  # Rotaciona 20 graus
+    time.sleep(0.01)                     # testar se resposta é exibida
+    print(f"Rotação enviada: {response}")
+    return frame
 
 def moves(tello, frame):
     '''
@@ -50,32 +50,30 @@ def moves(tello, frame):
 
     if detections > 0:
         searching = False
-        start_time = time.time()  # Reinicia o timer
-
-    if detections == 0 and old_move != 'land' and not searching:
-        if timer():  # Se 10 segundos se passaram sem detecção
+        start_time = time.time()
+    
+    elif detections == 0 and old_move != 'land':
+        if timer():
             searching = True
-            frame, detected = search(tello, frame, detections)  # Inicia a busca
-            if detected:
-                searching = False  # Sai da busca ao encontrar um QR code
+            frame = search(tello, frame)
+        elif old_move == 'follow': # necessário para que o drone não continue a se movimentar sem detecção de follow
+            tello.send_rc_control(0, 0, 0, 0)
 
-    if text == 'dados de leitura':
+    if text == 'follow':
         frame = tracking(tello, frame)
     
     if detections == 1 and text == 'land':
         while float(tello.get_state_field('h')) >= 13:
             tello.send_rc_control(0, 0, -70, 0)
-        tello.send_cmd(text)
-        print(text)
+        tello.send_cmd(str(text))
     
     elif detections == 1 and text == 'takeoff' and old_move != 'takeoff':
         response = tello.send_cmd_return(text)
-        print(text, response)
+        print(f"{text}' '{response}")
     
     elif detections == 1 and text in pace_moves:
         response = tello.send_cmd_return(f"{text}{pace}")
-        if response == 'ok':
-            print(f"{text}{pace}", response)
+        print(f"{text}{pace}' '{response}")
 
     old_move = text
     return frame
